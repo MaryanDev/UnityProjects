b4dc863146fc95cc5c719cec978c6b828f7d1d9b
Maryan Maykher <mr.maykher@gmail.com>
2022-10-27 23:54:34 +0300

    added gamemanager

---
+++ CubeGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            Debug.Log("Game over");
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }  
+
+    void Restart()
+    {
+        //SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
